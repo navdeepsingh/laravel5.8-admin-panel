fb74549f80fffb759791562b52540af5b7473a47
Navdeep Singh <navdeep.er@gmail.com>
2019-08-08 14:33:56 +0530

    Update

--- resources/js/components/Privacy.jsx
+++ resources/js/components/Privacy.jsx
@@ -10,25 +10,92 @@ class Terms extends React.Component {
         <div className="container">
           <div className="row py-3">
             <div className="col-md-12">
-              <h2>Privacy Policy</h2>
-              <h3>Brotzeit Oktoberfest Free Beer Campaign 2019</h3>
-              <ol>
-                <li>This offer is valid until 15th October 2019 only, unless otherwise stated. </li>
-                <li>Offer is valid only with a minimum order of 1 menu item per redemption. </li>
-                <li>Redemption is limited to 1 redemption per person on selected beers only, while stocks last. </li>
-                <li>Redemption is valid solely in-person at any of Brotzeit’s outlets and for dine-in only:
-                  <ul>
-                    <li>Brotzeit Vivocity</li>
-                    <li>Brotzeit Raffles City</li>
-                    <li>Brotzeit Westgate</li>
-                    <li>Brotzeit Katong</li>
-                    <li>Brotzeit 313 Somerset</li>
-                  </ul>
-                </li>
-                <li>This promotion is not valid with any other discounts, promotions, privilege cards or vouchers. The management reserves the rights to amend the terms and conditions without any prior notice.</li>
-                <li>Brotzeit Pte Ltd reserves the right to request for proof of identification at point of redemption and reserves the sole and absolute right to disqualify any participant or reject participants deemed ineligible for the promotion (be it under these Terms & Conditions or has engaged in a conduct that the organiser considers inappropriate or unacceptable). </li>
-                <li>Brotzeit Pte Ltd reserves the sole and absolute right to alter or end the offer at anytime, without giving prior notice or compensate in cash or in kind.</li>
+              <h2>PDPA</h2>
+              <p>This is the Brotzeit Pte Ltd Personal Data Protection Policy (“Personal Data Protection Policy”) and the purpose of this document is to inform you of Brotzeit Pte Ltd manages, collects, uses and discloses your Personal Data, which is subject to the Singapore Personal Data Protection Act (2012) (“the Act”). This Privacy Policy outlines Brotzeit Pte Ltd’s policy and responsibility in relation to the collection, use and disclosure of Customer Data.</p>
+              <p>By continuing to use Brotzeit Pte Ltd’s services, you signify that you have read, understood and agree to be bound by this Privacy Policy as amended from time to time in respect of Brotzeit Pte Ltd’s collection, use and disclosure of your Customer Data. This Privacy Policy is divided into two Parts – A and B:</p>
+              <p>Part A: General Customer Data Privacy Policy Statement</p>
+              <p>Part B: Privacy Policy Statement for Customer Data collected through Brotzeit Pte Ltd’s websites</p>
+
+              <h2>Part A: General Customer Data Privacy Policy Statement</h2>
+
+              <h3>1. Types of Customer Data</h3>
+              <p>The types of Customer Data that Brotzeit Pte Ltd collects depend on the circumstances of collection and on the nature of the service requested or transaction undertaken. There are two broad categories of Customer Data that Brotzeit Pte Ltd collects:</p>
+              <p>Personal Data, which includes but is not limited to -</p>
+              <ol type="i">
+                <li>personal information that links back to an individual e.g., name, gender, date of birth;</li>
+                <li>contact information e.g., address, phone number, email address;</li>
+                <li>technical information e.g., IP address; and </li>
               </ol>
+              <p>Statistical Data e.g., number of visitors, to website. This is stored purely for analytical purposes, and is entirely anonymous. This information will not be stored to your customer record, and will only be aggregated for statistical analysis so that we can better understand Brotzeit Pte Ltd's customer profile and improve Brotzeit Pte Ltd's service offering. </p>
+              <p>For purposes of this policy statement, the phrase Customer Data includes Personal Data and Statistical Data. The provision of Customer Data (save for where the request for data is marked “optional” in the forms used to collect personal data) is obligatory in order for Brotzeit Pte Ltd to carry out its services to you. The failure to supply such Customer Data may result in (i) Brotzeit Pte Ltd being unable to provide you with the services and/or products requested; (ii) Brotzeit Pte Ltd being unable to update you on our latest products and/or launches; and/or (iii) your inability to enter or participate in contests, promotions or redemption activities organised by Brotzeit Pte Ltd.  </p>
+
+              <h3>2. Purposes for Collection, Use and Disclosure</h3>
+              <p>Generally, Brotzeit Pte Ltd collects Customer Data, either directly from you or from third parties (e.g. our service provider), or from publicly available sources, through our website, mobile services, and other channels including our retail outlets, in order to fulfil the following purposes, which include but are not limited to:</p>
+              <ul>
+                <li>Providing services to you such as processing a transaction (e.g., making an order online), providing technical or customer service assistance, assisting you in the transaction through Brotzeit Pte Ltd’s website;</li>
+                <li>Marketing and communicating with you in relation to products and services offered by Brotzeit Pte Ltd, Brotzeit Pte Ltd’s service partners, as well as Brotzeit Pte Ltd’s appointed agents;</li>
+                <li>Contacting you for product or customer satisfaction surveys and market research;</li>
+                <li>Safety, security and legal compliance.</li>
+              </ul>
+              <p>Brotzeit Pte Ltd may also use and disclose your Customer Data with and to third parties in order to fulfil the purposes listed above. Brotzeit Pte Ltd shall use its best endeavours to ensure that its employees, officers, agents, consultants, contractors and such other third parties mentioned above who are involved in the collection, use and disclosure of Customer Data will observe and adhere to the terms of this Privacy Policy. Brotzeit Pte Ltd may disclose your Customer Data to law enforcement agencies and government for security, customs and immigration purposes.</p>
+              <p>In addition, Brotzeit Pte Ltd may disclose Customer Data to our legal advisors for establishing, exercising or defending our legal rights, to our other professional advisors, or as otherwise authorised or required by law. Brotzeit Pte Ltd also reserves the right to share Customer Data as is necessary to prevent a threat to the life, health or security of an individual or corporate entity such as Brotzeit Pte Ltd. Further, Brotzeit Pte Ltd may disclose Customer Data, as is necessary, to investigate suspected unlawful activities including but not limited to fraud, intellectual property infringement or privacy.  </p>
+
+              <h3>3. Transfer of Information Overseas</h3>
+              <p>The Brotzeit Pte Ltd Office is based in Singapore. Customer Data may be transmitted to data storage where Brotzeit Pte Ltd keeps its central records. Customer Data may be transferred to Brotzeit Pte Ltd’s offices and appointed agents in Singapore or other countries in connection with Brotzeit Pte Ltd’s performance of the contract with you.</p>
+
+              <h3>4. Consent </h3>
+              <p>Generally, in the course of Brotzeit Pte Ltd performing transactional functions such as making an order or booking online, signing up for our offers, Customer Data will, by nature of the task or transaction, be provided by our customers to Brotzeit Pte Ltd. In such instances, consent will necessarily be implied from the Customer that they are agreeable to providing Customer Data in order for Brotzeit Pte Ltd to provide the requested or necessary service and/or product to them. However, where required by law, Brotzeit Pte Ltd will adopt an ‘opt-in' policy for obtaining customer consent, in which event, express written consent will be sought from you when collecting your Customer Data e.g., signing a form or checking a box. Where you make reservations on behalf of another person, you undertake and will ensure that the individual whose Customer Data is supplied to Brotzeit Pte Ltd has authorized the disclosure, is informed of and consents to the terms and conditions of this Privacy Policy. You may submit a request to withdraw your consent at any time by contacting Brotzeit Pte Ltd (see Section 10). Statistical Data is not linked to a customer record and Brotzeit Pte Ltd does not need to seek consent for the collection, use or disclosure of Statistical Data. For more information on the collection and usage of Customer Data obtained from Brotzeit Pte Ltd’s websites facility, please refer to Part B.</p>
+
+              <h3>5. Access </h3>
+              <p>Brotzeit Pte Ltd will, upon your written request to our Data Protection Department (see Section 10), allow you to view your stored Personal Data. Where permitted by law, Brotzeit Pte Ltd reserves the right to charge a reasonable administrative fee for this service. In exceptional circumstances, Brotzeit Pte Ltd reserves the right to deny you access to your Personal Data and may provide an explanation as required by applicable laws. Exceptional circumstances include where: </p>
+              <ul>
+                <li>An investigating authority or government institution objects to Brotzeit Pte Ltd complying with a customer’s request,</li>
+                <li>The information may, in the exercise of Brotzeit Pte Ltd’s reasonable discretion and/or assessment, affect the life or security of an individual, and</li>
+                <li>Data is collected in connection with an investigation of a breach of contract, suspicion of fraudulent activities or contravention of law. </li>
+              </ul>
+              <p>To submit a written request to Customer Affairs Department to access your Personal Data, please email us at <a href="mailto:info@brotzeit.co" target="_blank">info@brotzeit.co</a>. </p>
+
+              <h3>6. Retention</h3>
+              <p>Brotzeit Pte Ltd will retain Customer Data for as long as it is necessary to fulfil the purpose for which it was collected, the legal or business purposes of Brotzeit Pte Ltd, or as required by relevant laws. When destroying Customer Data, we will take commercially reasonable and technically possible measures to make the personal information irrecoverable or irreproducible in accordance with the applicable laws.</p>
+
+              <h3>7. Accuracy</h3>
+              <p>Brotzeit Pte Ltd needs your assistance to ensure that your Personal Data is current, complete and accurate. As such, please inform Brotzeit Pte Ltd of changes to your Personal Data by contacting Brotzeit Pte Ltd and submitting your updated particulars to Brotzeit Pte Ltd in writing (see Section 10).</p>
+              <p>Brotzeit Pte Ltd may also request Personal Data updates from you from time to time. As detailed in Section 2 above under the “Purposes for Collection, Use and Disclosure” sub-section, your delivery address may be disclosed to the appropriate delivery fulfilment companies. As such, it is important to ensure that the Personal Data contained in your order confirmation is current, complete and accurate.</p>
+
+              <h3>8. Security Safeguards</h3>
+              <p>Brotzeit Pte Ltd takes the security and protection of your Customer Data very seriously. As such, Brotzeit Pte Ltd makes reasonable security arrangements to protect your Customer Data against loss or theft as well as unauthorised access and undue disclosure. Examples of technical safeguards include encryption, Network and Web Application Firewalls ,“firewalls” and Secure Socket Layer (SSL). Further details of these technical safeguards for Customer Data collected through Brotzeit Pte Ltd’s websites facility are set out at Part B below. If, however, a customer does not take reasonable care to ensure the continued confidentiality and accuracy of their Customer Data, Brotzeit Pte Ltd will not be liable for any consequential misuse and/or fraud. If you have any concerns about security, you should contact Brotzeit Pte Ltd (see Section 10).</p>
+
+              <h3>9. Updates to the Privacy Policy</h3>
+              <p>Brotzeit Pte Ltd will amend this Privacy Policy from time to time and the updated versions will be posted on Brotzeit Pte Ltd’s website and date stamped so that you are aware of when the Privacy Policy was last updated. Subject to applicable laws, the English version of this Privacy Policy will prevail over any version of this Privacy Policy in another language. In the event of any inconsistency in interpretation between the English version and any translation of the Privacy Policy, this Privacy Policy statement in English will prevail.</p>
+
+              <h3>10. Contact Us</h3>
+              <p>If you have comments, questions or complaints about or requests relating to this Privacy Policy statement, please contact Brotzeit Pte Ltd in writing at the address below referencing ‘Privacy Policy':</p>
+              <p>
+                DATA PROTECTION OFFICER (DPO):<br />
+                Paulina Oktari Halim
+              </p>
+              <p>
+                Group Marketing Manager<br />
+                Tel: +65 6222 9443  <br />
+                Email: <a href="mailto:PaulinaO@brotzeit.co" target="_blank">PaulinaO@brotzeit.co</a>
+              </p>
+
+              <p>If you have comments, questions or complaints about or requests relating to this Privacy Policy statement, please contact Brotzeit Pte Ltd at <a href="mailto:info@brotzeit.co" target="_blank">info@brotzeit.co</a>. </p>
+
+              <h2>Part B: Privacy Policy Statement for Customer Data collected through Brotzeit Pte Ltd's websites</h2>
+              <h3>1. Security Safeguards</h3>
+              <p>Brotzeit Pte Ltd takes the protection of your Customer Data collected on Brotzeit Pte Ltd’s websites very seriously. All Customer Data collected through Brotzeit Pte Ltd’s websites, e.g., http://www.brotzeit.co are protected by a secure server. In addition, SSL (Secure Socket Layer) protects the transmission of data from the internet to our systems. To further strengthen the integrity of your internet transactions on our websites, Brotzeit Pte Ltd also employs relevant software programs to monitor network traffic with a view to identifying unauthorised attempts to upload or change Customer Data, perpetuate fraudulent or illegal activities or otherwise cause damage. If such monitoring reveals evidence of possible abuse or criminal activities, such evidence may be provided to appropriate law enforcement authorities or agencies without notice to you. To give you secure access to our online services, arising from your utilisation of Brotzeit Pte Ltd’s website, you can register with us. You should not share your User ID or Password and should always ‘log off' once you have finished your session on the websites as well as take the recommended precautions that ensure the safe and secure provision of Brotzeit Pte Ltd’s website.</p>
+              <h3>2. Cookies</h3>
+              <p>Most websites like ours use cookies to enhance your online experience. Cookies are alphanumeric identifiers that are transferred to your computer's hard drive from your web browser to recognise your preferences and to tailor content to you. The cookies do not contain your Customer Data but only Statistical Data which is entirely anonymous. Please refer to your browsers documentation to check if cookies have been enabled on your computer or to request not to receive cookies. Information and procedures regarding cookie handling are provided by the respective browsers documentation within the browser itself by clicking on ‘help' and performing a search on ‘cookies'.</p>
+              <h3>3. Clickstream Data</h3>
+              <p>In order to improve your online experience, Brotzeit Pte Ltd may track online behaviour or clickstream data to advance your use of our web pages and track referrals from other websites. Such data will not be stored to your customer record, and will only be aggregated for statistical analysis.</p>
+              <h3>4. Links to Other Websites</h3>
+              <p>Brotzeit Pte Ltd provides you with links to other websites for your convenience and information. You access these websites at your own risk and Brotzeit Pte Ltd is not responsible for these websites. Whilst Brotzeit Pte Ltd will protect your Customer Data on Brotzeit Pte Ltd websites, Brotzeit Pte Ltd cannot control or be responsible for the policies of other sites we may link to, or the use of any Customer Data you may share with them. Please note that the Brotzeit Pte Ltd Privacy Policy does not cover these other websites, and Brotzeit Pte Ltd would recommend that you are apprised of their specific policies.</p>
+              <h3>5. Minors</h3>
+              <p>Brotzeit Pte Ltd cannot distinguish the age of persons who access and use its websites. If a minor (according to applicable laws) has provided Brotzeit Pte Ltd with Customer Data without parental or guardian consent, the parent or guardian should contact Brotzeit Pte Ltd (see Section 10 of Part A) to remove the relevant Customer Data and unsubscribe the minor.</p>
+              <h3>6. Google Analytics</h3>
+              <p>Google Analytics is a web analysis service provided by Google. Google utilizes the data collected to track and examine the use of www.wineconnection.com.sg, to prepare reports on its activities and share them with other Google services. Google may use the data collected to contextualize and personalize the ads of its own advertising network. For more details, please refer to Google’s Privacy Policy. </p>
+
             </div>
           </div>
         </div>

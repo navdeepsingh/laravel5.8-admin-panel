d3fdf74ffe6ab49fda479ea8c454fe6eae1651c6
Navdeep Singh <navdeep.er@gmail.com>
2019-08-22 17:12:40 +0530

    Update

--- resources/js/components/Thanks.jsx
+++ resources/js/components/Thanks.jsx
@@ -26,13 +26,11 @@ class Thanks extends React.Component {
                     <li className="list-inline-item"><a href={"https://twitter.com/intent/tweet?text=" + config.tweet}
                       rel="noopener" target="_blank"><i className="fa fa-twitter"></i></a></li>
                     <li className="list-inline-item"><a href={config.mail} target="_blank"><i className="fa fa-envelope"></i></a></li>
+                    <li className="list-inline-item"><a href={"https://api.whatsapp.com/send?text=" + config.whatsapp} target="_blank"><i className="fa fa-whatsapp"></i></a></li>
                   </ul>
-                  <p className="share-text">
-                    Share on Whatsapp <a href={"https://api.whatsapp.com/send?text=" + config.whatsapp} target="_blank">here</a>.
-                  </p>
                   <h4 className="pt-3 book-now">BOOK YOUR TABLE TODAY!</h4>
                   <a href="http://brotzeit.co/reservation/?utm_source=microsite&utm_medium=referral&utm_campaign=free_beer%20&utm_content=thankyou_page" target="_blank" className="btn btn-primary">BOOK NOW</a>
-                  <p className="pt-5 pb-2"><a href="http://brotzeit.co/promotion/oktoberfest-2019/?utm_source=microsite&utm_medium=referral&utm_campaign=free_beer&utm_content=thankyou_page" target="_blank"><img src="./images/2019-08-14_Brotzeit_OktoBeerThankYou_MainHeader_RO.jpg" className="img-fluid" /></a></p>
+                  <p className="pt-5 pb-2"><a href="http://brotzeit.co/promotion/oktoberfest-2019/?utm_source=microsite&utm_medium=referral&utm_campaign=free_beer&utm_content=thankyou_page" target="_blank"><img src="./images/2019-08-22_Brotzeit_OktoBeerThankYouEmail_MainHeader_800x350_RO.jpg" className="img-fluid" /></a></p>
                   <p className="py-2"><a href="http://brotzeit.co/promotion/oktoberfest-2019/?utm_source=microsite&utm_medium=referral&utm_campaign=free_beer&utm_content=thankyou_page" target="_blank"><img src="./images/2019-08-22_Brotzeit_OktoBeerCampaignThankYouPage_PromoBanner_RO.jpg" className="img-fluid" /></a></p>
                   <p>Click <a href="http://brotzeit.co/promotion/oktoberfest-2019/?utm_source=mailchimp&utm_medium=edm&utm_campaign=free_beer&utm_content=redemption_email" target="_blank">here</a> for more information on Oktoberfest Celebrations @ Brotzeit!</p>
                   <div className="text-center pt-5">
